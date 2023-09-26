79b5414ad001fbe0ba66eaaea9ae77b5e7f895e2
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2023-09-27 01:53:38 +0900

    ADD:ナビゲーションバーの追加

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    // 画像の取得を許可するドメイン
+    domains: ['placehold.jp']
+  },
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
