cef3dda461682f96f32b8334b0aea294fa298642
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2023-09-26 22:40:49 +0900

    ADD:prettierの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { signIn, signOut, useSession } from "next-auth/react";
-import Head from "next/head";
-import Link from "next/link";
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Head from 'next/head'
+import Link from 'next/link'
 
-import { api } from "~/utils/api";
+import { api } from '~/utils/api'
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const hello = api.example.hello.useQuery({ text: 'from tRPC' })
 
   return (
     <>
@@ -45,23 +45,23 @@ export default function Home() {
           </div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+              {hello.data ? hello.data.greeting : 'Loading tRPC query...'}
             </p>
             <AuthShowcase />
           </div>
         </div>
       </main>
     </>
-  );
+  )
 }
 
 function AuthShowcase() {
-  const { data: sessionData } = useSession();
+  const { data: sessionData } = useSession()
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
+    { enabled: sessionData?.user !== undefined },
+  )
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
@@ -73,8 +73,8 @@ function AuthShowcase() {
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
-        {sessionData ? "Sign out" : "Sign in"}
+        {sessionData ? 'Sign out' : 'Sign in'}
       </button>
     </div>
-  );
+  )
 }
